1e90ad368c6e022f94a5e367a923a95cd5b20f5d
Andrew Waller <andrewhwaller@gmail.com>
2019-04-20 21:41:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,8 +11,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @num = params[:number]
-    @num.to_i.times do
+    params[:number].to_i.times do
       "#{params[:phrase]}"
     end
   end
