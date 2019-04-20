65efc526bb318a68cb8308b6ff26748ded9ece68
Andrew Waller <andrewhwaller@gmail.com>
2019-04-20 21:43:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,13 +15,13 @@ class App < Sinatra::Base
       params[:phrase]
     end
   end
+  #
+  # get '/say/:word1/:word2/:word3/:word4/:word5' do
+  #
+  # end
 
-  get '/say/:word1/:word2/:word3/:word4/:word5' do
-
-  end
-
-  get '/:operation/:number1/:number2' do
-
-  end
+  # get '/:operation/:number1/:number2' do
+  #
+  # end
 
 end
