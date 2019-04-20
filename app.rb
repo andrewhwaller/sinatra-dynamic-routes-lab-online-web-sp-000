82524e13e9c25f60101196efea66bbd2114e42e5
Andrew Waller <andrewhwaller@gmail.com>
2019-04-20 21:51:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,7 +23,26 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
 
+    answer = 'I cannot make that happen...'
+
+    case params[:operation]
+    when 'add'
+      answer = (number1 + number2).to_s
+    when 'multiply'
+      answer = (number1 * number2).to_s
+    when 'subtract'
+      answer = (number1 - number2).to_s
+    when 'divide'
+      answer = (number1/number2).to_s
+      end
+    answer
+  end
+      
+    
+    "#{}"
   end
 
 end
